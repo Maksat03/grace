df4bd091c6eeec1522c8287e20b9672956f331a1
Maksat <54549462+Maksat03@users.noreply.github.com>
2023-10-03 14:12:56 +0600

    телеграм бот косылды

--- telegram bot/main.py
+++ telegram bot/main.py
@@ -1,39 +1,22 @@
 import logging
+
 from aiogram import Bot, Dispatcher, executor
 from aiogram.types import ContentType, Message
 
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token="6110400554:AAFMH8jOOwYzq4IwVRX5CwScxQRwBz66ivY", parse_mode='HTML')
+bot = Bot(token="6698535460:AAECdvYZGj_di-IXUtl9Fll0rE1l_WAZlE8", parse_mode='HTML')
 dp = Dispatcher(bot)
-
-
-def save_tg_id(tg_id):
-    tg_ids = []
-
-    try:
-        with open('../../settings/tg_ids.txt', 'r') as file:
-            for line in file:
-                tg_ids.append(line.strip())
-    except Exception as e:
-        print(e)
-
-    if not (str(tg_id) in tg_ids):
-        tg_ids.append(str(tg_id))
-        with open('../../settings/tg_ids.txt', 'w') as file:
-            file.write("\n".join(tg_ids))
 
 
 @dp.message_handler(commands=['start'])
 async def welcome(msg: Message):
     await msg.answer(msg.chat.id)
-    save_tg_id(msg.chat.id)
 
 
 @dp.message_handler(content_types=ContentType.TEXT)
 async def get_message(msg: Message):
     await msg.answer(msg.chat.id)
-    save_tg_id(msg.chat.id)
 
 
 if __name__ == "__main__":
